6544a0ab603d637d47dda94a4a67773311396efc
hugobardales <vhbardalesp@gmail.com>
2025-07-02 00:07:31 -0500

    Agregando nueva funcionalidad, un menu dinámico que maneja errores del usuario

--- src/Cinema/Cinema.java
+++ src/Cinema/Cinema.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 public class Cinema {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int ticketBought;
+        int currentIncome;
+
+        int continueInnerLoop = 1;
+        int continueOuterLoop = 1;
+        int outerLoop = 1;
+
+
+        // Creates and instantiated a object name cinema
+        PrintCinema printCinema = new PrintCinema();
+
 
         System.out.println("Enter the number of rows:");
         int rows = scanner.nextInt();
@@ -22,40 +33,86 @@ public class Cinema {
             }
         }
 
-        // Creates and instantiated a object name cinema
-        PrintCinema printCinema = new PrintCinema();
-        printCinema.printSeatCinema(seatsCinema);
 
-        System.out.println("Enter a row number:");
-        int rowNum = scanner.nextInt();
+        do {
+            switch (choiceMenu(scanner)) {
+                case 1:
+                    do {
+                        printCinema.printSeatCinema(seatsCinema);
 
-        System.out.println("Enter a seat number in that row:");
-        int seatRow = scanner.nextInt();
+                        System.out.println("Continue with this choice ? (Enter 1 for yes/ 2 for no):");
+                        continueInnerLoop = scanner.nextInt();
+                        break;
 
-        // Body which calculate the price of seats
-        int totalAsientos = rows * seatsInRow;
-        int ticketPrice = 0;
+                    } while (continueInnerLoop == 1);
 
-        if (totalAsientos <= 60) {
-            ticketPrice = 10;
-        } else if (totalAsientos > 60) {
-            int mitadFilas = rows / 2;
-            int filasParteFrontal = mitadFilas;
-            if (rowNum <= filasParteFrontal) {
-                ticketPrice = 10;
-            } else {
-                ticketPrice = 8;
+                case 2:
+
+                    do {
+                        int rowNum = 0;
+                        int seatRow = 0;
+
+                        System.out.println("Enter a row number:");
+                        rowNum = scanner.nextInt();
+
+                        System.out.println("Enter a seat number in that row:");
+                        seatRow = scanner.nextInt();
+
+                        if (rowNum > rows || seatRow > seatsInRow) {
+                            System.out.println("Wrong input. Enter a valid parameter minor that " + rows + " " + seatsInRow);
+                            continue;
+
+                        }
+
+                        if () {
+                            // Refresh the map seats of Cinema
+                            seatsCinema[rowNum - 1][seatRow - 1] = 'B';
+
+                            // Body which calculate the price of seats
+                            int totalAsientos = rows * seatsInRow;
+                            int ticketPrice = 0;
+
+                            if (totalAsientos <= 60) {
+                                ticketPrice = 10;
+                            } else if (totalAsientos > 60) {
+                                int mitadFilas = rows / 2;
+                                int filasParteFrontal = mitadFilas;
+                                if (rowNum <= filasParteFrontal) {
+                                    ticketPrice = 10;
+                                } else {
+                                    ticketPrice = 8;
+                                }
+                            }
+                            System.out.println("Ticket price: $" + ticketPrice);
+                        }
+
+                        System.out.println("Continue with this choice ? (Enter 1 for yes/ 2 for no):");
+                        continueInnerLoop = scanner.nextInt();
+
+                    } while (continueInnerLoop == 1);
+
             }
-        }
-        System.out.println("Ticket price: $" + ticketPrice);
+            System.out.println("Continue with this choice ? (Enter 1 for yes/ 2 for no):");
+            continueOuterLoop = scanner.nextInt();
 
+        } while (continueOuterLoop == 1);
 
-        // Refresh the map seats of Cinema
-        seatsCinema[rowNum - 1][seatRow - 1] = 'B';
-
-        System.out.println();
-        printCinema.printSeatCinema(seatsCinema);
 
     }
 
+    public static int choiceMenu (Scanner scanner) {
+        int choiceGivenUser;
+
+        System.out.println("1. Show seats");
+        System.out.println("2. Buy a ticket");
+        System.out.println("3. Statistics");
+        System.out.println("0. Exit ");
+
+        // The user choose a number 1 to 3 and 0 from the scanner input
+        choiceGivenUser = scanner.nextInt();
+
+        // Return a value
+        return choiceGivenUser;
+
+    }
 }
